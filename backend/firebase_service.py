bf74c3793c6e279df33e2927f147a69df2ed3a71
wilson <whlew2003@gmail.com>
2026-05-26 11:44:13 +0800

    test decision

--- backend/firebase_service.py
+++ backend/firebase_service.py
@@ -284,6 +284,6 @@ class FirebaseService:
             vector_field="embedding_vector",
             query_vector=Vector(line_vector),
             distance_measure=DistanceMeasure.COSINE,
-            limit=100,
+            limit=50,
             distance_result_field="vector_distance"
         )
---
+++ backend/main.py
@@ -0,0 +1,4 @@
+import requirement_backend
+import decision_backend
+
+from general import app
